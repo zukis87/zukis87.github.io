03fa04e47982bbb50d2fce36e2de248f7c553571
Zuki <zuriel.saru@zooz.com>
2019-02-02 15:00:27 +0200

    test 1

---
+++ precache-manifest.ef15b282792fe2f26f61c308ab1d9ea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "571458d76b34770ac529",
+    "url": "/static/js/main.571458d7.chunk.js"
+  },
+  {
+    "revision": "361838558ad1b05dfcd5",
+    "url": "/static/js/1.36183855.chunk.js"
+  },
+  {
+    "revision": "571458d76b34770ac529",
+    "url": "/static/css/main.b93dd798.chunk.css"
+  },
+  {
+    "revision": "a67bc47697780027777153652002e350",
+    "url": "/index.html"
+  }
+];
